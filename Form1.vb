c7aa5420f0bda1f00bd459ab018d5f04d59f57ca
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2022-10-01 09:56:31 +0800

    Test 3

--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,3 @@
 ﻿Public Class fkyoubtn
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
 End Class
