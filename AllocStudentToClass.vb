4b7717f9afa0de20fcccde8e77c1ec63eb69b4f4
Jhun Kit <jhunkit2753@gmail.com>
2022-10-30 02:50:47 +0800

    Error Handling for multiple primary key sucks, Error handling for Enrolled complete

--- AllocStudentToClass.vb
+++ AllocStudentToClass.vb
@@ -8,12 +8,32 @@ Public Class AllocStudentToClass
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         Dim studentId As Integer = comStudentId.SelectedItem
         Dim classId As String = "'" & comClassId.SelectedItem.ToString & "'"
+        Dim stuIdAndClassIdData As New SqlDataAdapter(New SqlCommand("Select * FROM enrolled WHERE studentId = " & studentId & "AND classId = " & classId & ";", Form2.connection))
+        Dim datatable As New DataTable()
+        stuIdAndClassIdData.Fill(datatable)
 
-        Dim insertQuery As String = "INSERT INTO enrolled VALUES (" & studentId & "," & classId & ");"
-        Form2.run_query(insertQuery)
-        Form2.table_load("enrolled")
 
-        MessageBox.Show("Student " & studentId & " has been added to class " & classId & ".", "Student allocated", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        If Not (datatable.Rows.Count > 0) Then
+            ' if there is no existing data that has the same combination of studentId and classId inputted, create new datatable
+            Dim stuIdData As New SqlDataAdapter(New SqlCommand("Select studentId FROM enrolled WHERE studentId = " & studentId & ";", Form2.connection))
+            Dim datatable2 As New DataTable()
+            stuIdData.Fill(datatable2)
+
+            If Not (datatable2.Rows.Count > 0) Then
+                ' if studentId does not already exist in the table, create new data
+                Dim insertQuery2 As String = "INSERT INTO enrolled VALUES (" & studentId & "," & classId & ");"
+                Form2.run_query(insertQuery2)
+                Form2.table_load("enrolled")
+
+                MessageBox.Show("Student " & studentId & " has been added to class " & classId & ".", "Student allocated", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ElseIf comStudentId.SelectedItem.ToString = datatable2.Rows(0).Item(0).ToString Then
+                ' if studentId already exist in the table, tell user to remove the data to reassign
+                MessageBox.Show("Student ID already assiged to a class. Please remove the student from the current class he/she is in to assign them into a new class", "Student ID Already Assigned To A Class", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        ElseIf comStudentId.SelectedItem.ToString = datatable.Rows(0).Item(0).ToString And comClassId.SelectedItem.ToString = datatable.Rows(0).Item(1).ToString Then
+            ' if there is an existing data that has the same combination of studentId and classId inputted, inform user that student already exist in the class
+            MessageBox.Show("Student ID already exists in the class.", "Student ID Already Exists In Class", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
     End Sub
 
     Private Sub AllocStudentToClass_Load(sender As Object, e As EventArgs) Handles MyBase.Load
