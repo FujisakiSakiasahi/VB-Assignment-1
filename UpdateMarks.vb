a9d435806af0f647cac5705296741223138e87d8
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2022-10-30 18:23:21 +0800

    Bug fix: Selection error

--- UpdateMarks.vb
+++ UpdateMarks.vb
@@ -39,12 +39,22 @@ Public Class UpdateMarks
     End Sub
 
     Private Sub comSubjectId_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comSubjectId.SelectedIndexChanged
-        Dim da As New SqlDataAdapter(New SqlCommand("SELECT * FROM marks where studentId = '" & comStudentId.SelectedItem & "' AND subjectId = '" & comSubjectId.SelectedItem & "';", Form2.connection))
+        Dim da As New SqlDataAdapter(New SqlCommand("SELECT distinct examyear FROM marks where studentId = '" & comStudentId.SelectedItem & "' AND subjectId = '" & comSubjectId.SelectedItem & "';", Form2.connection))
         Dim datatable As New DataTable()
         da.Fill(datatable)
 
-        txtExamYear.Text = datatable.Rows(0).Item(2)
-        txtMark.Text = datatable.Rows(0).Item(3)
+        For i As Integer = 0 To datatable.Rows.Count - 1
+            comExamYear.Items.Add(datatable.Rows(i).Item(0))
+        Next
+
+        comExamYear.SelectedIndex = 0
+    End Sub
+    Private Sub comExamYear_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comExamYear.SelectedIndexChanged
+        Dim da As New SqlDataAdapter(New SqlCommand("SELECT mark FROM marks where studentId = '" & comStudentId.SelectedItem & "' AND subjectId = '" & comSubjectId.SelectedItem & "' AND examyear = " & comExamYear.SelectedItem & ";", Form2.connection))
+        Dim datatable As New DataTable()
+        da.Fill(datatable)
+
+        txtMark.Text = datatable.Rows(0).Item(0)
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
@@ -52,30 +62,25 @@ Public Class UpdateMarks
     End Sub
 
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
-        If String.IsNullOrEmpty(txtExamYear.Text) Or String.IsNullOrEmpty(txtMark.Text) Then
+        If String.IsNullOrEmpty(txtMark.Text) Then
             MessageBox.Show("One or more fields are empty. Please provide information for these fields.", "Empty Fields", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-        ElseIf IsNumeric(txtExamYear.Text) = False Then
-            MessageBox.Show("Exam Year should not include characters or symbols. Please enter number in range of 2000 - 3000.", "Invalid Exam Year", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         ElseIf IsNumeric(txtMark.Text) = False Then
             MessageBox.Show("Marks should not include characters or symbols. Please enter number in range of 0 - 100.", "Invalid Marks", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         Else
-            Dim examYear As Integer = txtExamYear.Text
-            If examYear < 2000 Or examYear > 3000 Then
-                MessageBox.Show("Exam year out of range. Please enter number in range of 2000 - 3000.", "Invalid Exam Year", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Dim examYear As Integer = comExamYear.SelectedIndex
+            Dim mark As Integer = txtMark.Text
+            If mark < 0 Or mark > 100 Then
+                MessageBox.Show("Mark out of range. Please enter number in range of 0 - 100.", "Invalid Mark", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Else
-                Dim mark As Integer = txtMark.Text
-                If mark < 0 Or mark > 100 Then
-                    MessageBox.Show("Mark out of range. Please enter number in range of 0 - 100.", "Invalid Mark", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                Else
-                    Dim studentId As Integer = comStudentId.SelectedItem
-                    Dim subjectId As String = "'" & comSubjectId.SelectedItem.ToString & "'"
-                    Dim insertQuery As String = "update marks set examyear = " & examYear & ", mark = " & mark & " where studentId = " & studentId & "AND subjectId = " & subjectId & " ;"
-                    Form2.run_query(insertQuery)
-                    Form2.table_load("marks")
+                Dim studentId As Integer = comStudentId.SelectedItem
+                Dim subjectId As String = "'" & comSubjectId.SelectedItem.ToString & "'"
+                Dim insertQuery As String = "update marks set examyear = " & examYear & ", mark = " & mark & " where studentId = " & studentId & "AND subjectId = " & subjectId & " ;"
+                Form2.run_query(insertQuery)
+                Form2.table_load("marks")
 
-                    MessageBox.Show("Mark record of student with student id " & studentId & " for subject with subject id " & comSubjectId.SelectedItem.ToString & " has been updated", "Mark Record Updated", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                End If
+                MessageBox.Show("Mark record of student with student id " & studentId & " for subject with subject id " & comSubjectId.SelectedItem.ToString & " has been updated", "Mark Record Updated", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
         End If
     End Sub
+
 End Class
